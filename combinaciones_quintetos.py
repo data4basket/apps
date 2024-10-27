fcf12f1c6540f392be0b8ca849f4fcb5fa2ecc53
data4basket <155992550+data4basket@users.noreply.github.com>
2024-10-27 08:58:42 +0100

    Update combinaciones_quintetos.py

--- combinaciones_quintetos.py
+++ combinaciones_quintetos.py
@@ -5,6 +5,9 @@ import streamlit as st
 import mysql.connector
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import date
+
+from Scripts_auxiliares/radarChart import *
 
 DB_user = st.secrets["DB_user"] 
 DB_host = st.secrets["DB_host"] 
@@ -13,93 +16,6 @@ DB_database = st.secrets["DB_database"]
 DB_port = st.secrets["DB_port"]
 Access_DB = {'DB_user': DB_user, 'DB_host': DB_host, 'DB_password': DB_password, 'DB_database': DB_database, 'DB_port': DB_port}
 
-##############################################################################################################################################
-##############################################################   RADAR CHART   ###############################################################
-##############################################################################################################################################
-
-def _invert(x, limits):
-    #inverts a value x on a scale from
-    #limits[0] to limits[1]
-    return limits[1] - (x - limits[0])
-
-
-def _scale_data(data, ranges):
-    #scales data[1:] to ranges[0],
-    #inverts if the scale is reversed
-    for d, (y1, y2) in zip(data[1:], ranges[1:]):
-        assert (y1 <= d <= y2) or (y2 <= d <= y1)
-    x1, x2 = ranges[0]
-    d = data[0]
-    if x1 > x2:
-        d = _invert(d, (x1, x2))
-        x1, x2 = x2, x1
-    sdata = [d]
-    for d, (y1, y2) in zip(data[1:], ranges[1:]):
-        if y1 > y2:
-            d = _invert(d, (y1, y2))
-            y1, y2 = y2, y1
-        sdata.append((d - y1) / (y2 - y1)
-                     * (x2 - x1) + x1 if (y2 - y1) != 0 else d)
-    return sdata
-
-
-class ComplexRadar():
-    def __init__(self, fig, variables, ranges,
-                 n_ordinate_levels=6):
-        angles = np.arange(0, 360, 360. / len(variables))
-
-        axes = [fig.add_axes([0.05, 0.05, 0.9, 0.9], polar=True,
-                             label="axes{}".format(i))
-                for i in range(len(variables))]
-        l, text = axes[0].set_thetagrids(angles,
-                                         labels=variables,
-                                         fontsize=14,
-                                         rotation=angles[1],#-90,
-                                         color='purple'
-                                         )
-
-        [txt.set_position((-0.05, -0.05)) for txt, angle in zip(text, angles)]
-        [txt.set_rotation(angle - 90) for txt, angle in zip(text, angles)]
-        for ax in axes[1:]:
-            ax.patch.set_visible(False)
-            ax.grid("off")
-            ax.xaxis.set_visible(False)
-        for i, ax in enumerate(axes):
-            grid = np.linspace(*ranges[i],
-                               num=n_ordinate_levels)
-            gridlabel = ["{}".format(round(x, 2))
-                         for x in grid]
-            for gL in range(len(gridlabel)):
-                if (gL != len(gridlabel)-1):
-                    gridlabel[gL] = ""  # clean up origin
-                else:
-                    gridlabel[gL] = int(float(gridlabel[gL]))
-
-            ax.set_rgrids(grid, labels=gridlabel,
-                          angle=angles[i])
-            ax.spines["polar"].set_visible(True)
-            ax.set_ylim(*ranges[i])
-        # variables for plotting
-        self.angle = np.deg2rad(np.r_[angles, angles[0]])
-        self.ranges = ranges
-        self.ax = axes[0]
-
-    def plot(self, data, legend, n_quintetos, *args, **kw):
-        sdata = _scale_data(data, self.ranges)
-        self.ax.plot(self.angle, np.r_[sdata, sdata[0]], label=legend, marker='o', ms=3, linewidth=1.5, alpha=0.5, *args, **kw)
-        for an in range(len(data)):
-            txt = str(data[an]) if type(data[an]) == "<class 'float'>" else str(round(data[an],1))
-            self.ax.annotate(txt, xy = (self.angle[an], np.r_[sdata[an], sdata[0]][0]+sdata[0]*0.025), annotation_clip=True, *args, **kw )
-        self.ax.legend(ncols=1, bbox_to_anchor=(0, 1.11 +0.05*n_quintetos),
-              loc='upper left', fontsize='large')
-
-    def fill(self, data, *args, **kw):
-        sdata = _scale_data(data, self.ranges)
-        self.ax.fill(self.angle, np.r_[sdata, sdata[0]], *args, **kw)
-
-##############################################################################################################################################
-##############################################################   RADAR CHART   ###############################################################
-##############################################################################################################################################
 
 
 #@st.cache_resource
@@ -163,7 +79,7 @@ def buscarJugadores(id_team):
     return df_out
 
 @st.cache_data
-def buscarStatFives(id_team, list_players_in, list_player_out):
+def buscarStatFives(id_team, list_players_in, list_player_out, RANGO_FECHAS):
     SQL_PLAYERS_IN = ""
     for player in list_players_in:
         SQL_PLAYERS_IN = SQL_PLAYERS_IN + "AND name_five like '%" + player + "%'"
@@ -172,8 +88,11 @@ def buscarStatFives(id_team, list_players_in, list_player_out):
         SQL_PLAYERS_OUT = SQL_PLAYERS_OUT + "AND name_five not like '%" + player + "%'"
         
     [conexion, _] = conectar_BDD()
+    SQL_RANGO_FECHAS = ""
     query = "SELECT * FROM j_fives WHERE num_players = 5 AND id_team = '" + id_team + "' " + SQL_PLAYERS_IN + SQL_PLAYERS_OUT
     df_out = pd.read_sql(query,conexion)
+    df_out['start_date']= pd.to_datetime(df_out['start_date'])
+    df_out = df_out[(df_out['start_date'] >= RANGO_FECHAS[0]) & (df_out['start_date'] <= RANGO_FECHAS[1])]
     conexion.close()
     return df_out
 
@@ -278,7 +197,7 @@ def PROCESAR():
     list_fives_df = []
     list_index_fives_df = []
     for j in range(selected_number_quintetos):
-        df_statFives_team = buscarStatFives(LIST_QUINTETOS_TODOS[j]['id_team'], LIST_QUINTETOS_TODOS[j]['players_IN'], LIST_QUINTETOS_TODOS[j]['players_OUT'])
+        df_statFives_team = buscarStatFives(LIST_QUINTETOS_TODOS[j]['id_team'], LIST_QUINTETOS_TODOS[j]['players_IN'], LIST_QUINTETOS_TODOS[j]['players_OUT'], RANGO_FECHAS)
 
         TextPlayersIn_team1 = " - ".join(str(element) for element in LIST_QUINTETOS_TODOS[j]['players_IN'])
         TextPlayersOut_team1 = " - ".join(str(element) for element in LIST_QUINTETOS_TODOS[j]['players_OUT'])
@@ -446,6 +365,7 @@ with colMain1:
                         )
     list_posible_stats = obj_type_stats[selected_type_stats]  
 
+
 with colMain2:
     if selected_type_stats != "***Personalizadas*** :movie_camera:": 
         list_selected_stats = st.multiselect(
@@ -476,6 +396,15 @@ with colMain2:
                                                         "A 40 minutos de juego (por partido)"
                                                     ])
 
+RANGO_FECHAS = st.slider(
+    "FILTRAR POR PARTIDOS ENTRE FECHAS",
+    min_value = date(2022, 9,1), max_value  = date.today(),
+    value=(date(2022, 9,1), date.today()),
+    format="DD/MM/YY"
+)
+
+
+RANGO_FECHAS = [pd.Timestamp(RANGO_FECHAS[0]), pd.Timestamp(RANGO_FECHAS[1])]
 st.text("")
 
 colMainB1, colMainB2, colMainB3= st.columns([2, 8, 2])
@@ -593,7 +522,7 @@ if st.session_state.get("procesado", False):
             JUG_OUT = DF[index]['JUG. OUT']
             txt_JUG_IN = '     * IN: ' + JUG_IN if JUG_IN != '' else ''
             txt_JUG_OUT = '     * OUT: ' + JUG_OUT if JUG_OUT != '' else ''
-            legend = DF[index]['EQUIPO'] + ': ' + txt_JUG_IN + txt_JUG_OUT
+            legend = DF[index]['EQUIPO']   + " : " + txt_JUG_IN + txt_JUG_OUT
             data = []
             for var in list_variables_a40mins:
                 data.append(row[var])
